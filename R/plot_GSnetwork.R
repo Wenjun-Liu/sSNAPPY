c851b9c22daa94a9850ad6448813f50ef0b03ecb
Stevie Ped <stephen.pederson.au@gmail.com>
2023-04-01 01:51:55 +1030

    Significant mods to plotting functions. Tests failing

--- R/plot_GSnetwork.R
+++ R/plot_GSnetwork.R
@@ -1,182 +1,230 @@
 #' @title Plot significantly perturbed gene-sets as a network
 #'
-#' @param normalisedScores A `data.frame` of pathway perturbation scores derived from the `normalise_by_permu()` function
-#' @param gsTopology List of pathway topology matrices generated using function `retrieve_topology()`
-#' @param colorBy Choose to color nodes either by *robustZ* or *pvalue*. A column must exist in the `normalisedScores`
-#' `data.frame` for the chosen parameter
-#' @param foldGSname `logical` Should long gene-set names be folded into two lines
-#' @param foldafter The number of words after which gene-set names should be folded. Defaulted to 2
-#' @param layout The layout algorithm to apply. Accept all layout supported by `igraph`
-#' @param edgeAlpha `numerical` Transparency of edges. Default to 0.8
-#' @param up_col The color used to label activated gene-sets. Only applicable if `colorBy` is set to be "robustZ"
-#' @param down_col The color used to label inhibited gene-sets. Only applicable if `colorBy` is set to be "robustZ"
-#' @param scale_edgeWidth A numerical vector of length 2 to be provided to `ggraph::scale_edge_width_continuous()` for specifying
-#' the minimum and maximum edge widths after transformation. Defaulted to `c(0.5, 3)`
-#' @param edgeLegend logical` Should edge weight legend be shown
-#' @param scale_nodeSize A numerical vector of length 2 to be provided to `ggplot2::scale_size()` for specifying
-#' the minimum and maximum node sizes after transformation. Defaulted to `c(3,6)`
+#' @param normalisedScores A `data.frame` of pathway perturbation scores derived
+#' from the `normalise_by_permu()` function
+#' @param gsTopology List of pathway topology matrices generated using function 
+#' `retrieve_topology()`
+#' @param colorBy Choose to color nodes either by *robustZ* or *pvalue*. A 
+#' column must exist in the `normalisedScores` `data.frame` for the chosen 
+#' parameter
+#' @param foldGSname logical(1) Should long gene-set names fold across multiple
+#' lines
+#' @param foldafter The number of words after which gene-set names should be 
+#' folded. Defaults to 2
+#' @param layout The layout algorithm to apply. Accept all layout supported by 
+#' `igraph`
+#' @param edgeAlpha numeric(1) Transparency of edges. Default to 0.8
+#' @param scale_edgeWidth A numerical vector of length 2 to be provided to 
+#' `ggraph::scale_edge_width_continuous()` for specifying
+#' the minimum and maximum edge widths after transformation. Defaults to 
+#' `c(0.5, 3)`
+#' @param edgeLegend logical(1) Should edge weight legend be shown
+#' @param scale_nodeSize A numeric vector of length 2 to be provided to 
+#' `ggplot2::scale_size()` for specifying the minimum and maximum node sizes 
+#' after transformation. Defaulted to `c(3,6)`
 #' @param nodeShape Shape of nodes
-#' @param color_lg `logical` Should color legend be shown
-#' @param color_lg_title Optional. Title for the color legend
+#' @param color_lg logical(1) Should color legend be shown
+#' @param color_lg_title Optional title for the color legend
 #' @param lb_size Size of node text labels
 #' @param lb_color Color of node text labels
-#' @param plotIsolated `logical` Should nodes not connected to any other nodes be plotted.  Default to FALSE
-#' @importFrom ggraph ggraph geom_node_point geom_node_text geom_edge_link
-#' @importFrom ggplot2 scale_color_continuous scale_color_manual aes_ guides aes scale_size
-#' @importFrom ggraph scale_edge_width_continuous
+#' @param plotIsolated logical(1) Should nodes not connected to any other nodes 
+#' be plotted.  Defaults to FALSE
+#' @param max_overlaps passed to \link[ggraph]{geom_node_text}
+#' @param ... Not used
+#' 
 #' @return A ggplot2 object
-#' @export
 #'
 #' @examples
 #' load(system.file("extdata", "gsTopology.rda", package = "sSNAPPY"))
 #' load(system.file("extdata", "normalisedScores.rda", package = "sSNAPPY"))
 #'
-#' #Subset pathways significantly perturbed in sample R5020_N2_48
-#' subset <- dplyr::filter(normalisedScores, adjPvalue < 0.05, sample == "R5020_N2_48")
+#' # Subset pathways significantly perturbed in sample R5020_N2_48
+#' subset <- dplyr::filter(
+#'   normalisedScores, adjPvalue < 0.05, sample == "R5020_N2_48"
+#' )
+#' subset[["status"]] <- ifelse(subset[["robustZ"]]> 0, "Activated", "Inhibited")
+#' 
 #'
-#' # Color network plot nodes by robust z-score
+#' # Color network plot nodes by status
 #' plot_gs_network(subset, gsTopology,
-#' colorBy = "robustZ", layout = "dh",
+#' colorBy = "status", layout = "dh",
 #' color_lg_title = "Direction of pathway Perturbation")
 #'
 #' # Color network plot nodes by p-values
 #' plot_gs_network(subset, gsTopology, layout = "dh",
 #' colorBy = "pvalue", color_lg_title = "P-value")
-plot_gs_network <- function(normalisedScores, gsTopology, colorBy = c("robustZ", "pvalue"), foldGSname = TRUE, foldafter = 2, layout = "fr",
-                           edgeAlpha = 0.8,  up_col = "brown3", down_col = "steelblue3", scale_edgeWidth = c(0.5, 3), edgeLegend = FALSE, scale_nodeSize = c(3,6),
-                           nodeShape = 16, color_lg = TRUE, color_lg_title = NULL, lb_size = 3, lb_color = "black", plotIsolated = FALSE){
-
+#' 
+#' @importFrom ggraph ggraph geom_node_point geom_node_text geom_edge_link
+#' @import ggplot2 
+#' @export
+plot_gs_network <- function(
+        normalisedScores, gsTopology, colorBy = NULL, 
+        foldGSname = TRUE, foldafter = 2, layout = "fr", edgeAlpha = 0.8,  
+        scale_edgeWidth = c(0.5, 3), edgeLegend = FALSE, scale_nodeSize = c(3,6),
+        nodeShape = 16, color_lg = TRUE, color_lg_title = NULL, lb_size = 3, 
+        lb_color = "black", plotIsolated = FALSE, max_overlaps = 10, ...
+){
+    
     name <- weight <- color <- size <- NULL
     ## check if input has required columns
-    stopifnot(colorBy %in% c("robustZ", "pvalue"))
-    if (!all(c(colorBy, "gs_name") %in% colnames(normalisedScores))) stop("Normalised Scores must include gs_name and column for coloring")
-
+    cols <- colnames(normalisedScores)
+    if (!is.null(colorBy)) colorBy <- match.arg(colorBy, cols)
+    
+    if (!"gs_name" %in% cols) stop("Normalised Scores must include gs_name")
+    
     # Make sure the gs topologies are a named list with at least two elements
     stopifnot(length(names(gsTopology)) == length(gsTopology))
-    if(length(unique(normalisedScores$gs_name)) < 2) stop("At least 2 gene-sets are required for a network plot")
+    if(length(unique(normalisedScores$gs_name)) < 2) 
+        stop("At least 2 gene-sets are required for a network plot")
     gsTopology <- gsTopology[names(gsTopology) %in% normalisedScores$gs_name]
-
+    
     # create igraph object
-    g <- make_gsNetwork(normalisedScores, gsTopology, colorBy = colorBy, plotIsolated = plotIsolated)
-
-    if(foldGSname){
-        g <- set_vertex_attr(g, "name", value = vapply(V(g)$name, function(x){ifelse(length(strsplit(x, " ")[[1]]) > foldafter,
-                                                                                     str_replace_nth(x, " ", "\n", foldafter),
-                                                                                     x)}, character(1))) }
-
+    g <- make_gsNetwork(
+        normalisedScores, gsTopology, colorBy = colorBy, 
+        plotIsolated = plotIsolated
+    )
+    
+    if (foldGSname){
+        nm <-  str_replace_nth(
+            V(g)$name, pattern = " ", replacement = "\n", n = foldafter
+        )
+        g <- set_vertex_attr(g, "name", value = nm)
+    }
+    
     # plot network edges
     pl <- ggraph(g, layout = layout) +
         geom_edge_link(alpha = edgeAlpha, aes(width=weight), colour='darkgrey') +
         scale_edge_width_continuous(range = scale_edgeWidth, guide = "none")
-
-
-    if (colorBy == "robustZ"){
-        pl <- pl + geom_node_point(aes(color = color, size = size), shape = nodeShape,stroke = 0.5) +
-            scale_color_manual(values = c("Activated" = up_col, "Inhibited" = down_col),name = color_lg_title) +
-            scale_size(range =  scale_nodeSize, guide = "none")
-    } else(
+    
+    
+    if (!is.null(colorBy)) {
+        pl <- pl + 
+            geom_node_point(
+                aes(color = color, size = size), shape = nodeShape, stroke = 0.5
+            ) +
+            scale_size(range =  scale_nodeSize, guide = "none") +
+            labs(colour = color_lg_title)
+        if(!color_lg) pl <- pl + guides(color = "none")
+    } else {
         pl <- pl +
-            geom_node_point(aes(color = color, size = size), shape = nodeShape,stroke = 0.5) +
-            scale_color_continuous(low="red", high="blue", name = color_lg_title) +
-            scale_size(range =  scale_nodeSize, guide = "none"))
-
-    if(!color_lg){
-     pl <- pl + guides(color = "none")
+            geom_node_point(aes(size = size), shape = nodeShape,stroke = 0.5) +
+            scale_size(range =  scale_nodeSize, guide = "none")
     }
-
-
-    pl + geom_node_text(aes(label = name), size = lb_size, repel = TRUE, colour = lb_color)
-
+    
+    pl + 
+        geom_node_text(
+            aes(label = name), size = lb_size, repel = TRUE, colour = lb_color,
+            max.overlaps = max_overlaps
+        ) 
+    
 }
+
 
 #' @importFrom reshape2 melt
 #' @importFrom dplyr bind_rows
-#' @importFrom igraph E V graph.data.frame set_edge_attr set_vertex_attr degree delete_vertices delete_edges
-make_gsNetwork <- function(normalisedScores, gsTopology,  colorBy = c("robustZ", "pvalue", "community"),
-                           plotIsolated ){
-
+#' @import igraph 
+#' @keywords internal
+make_gsNetwork <- function(
+        normalisedScores, gsTopology, colorBy = NULL, plotIsolated
+){
+    
     # create dummy variable to pass R CMD CHECK
-    from <- to <- E <- robustZ <- NULL
+    from <- to <- NULL
     GS2Gene <- get_GSgenelist(gsTopology)
-    GS2Gene <- left_join(normalisedScores, GS2Gene, by = "gs_name")
-
+    GS2Gene <- left_join(
+        normalisedScores, GS2Gene, by = "gs_name", multiple = "all"
+        ## Setting multiple = "all" requires dplyr > 1.1.0
+    )
+    
     GSlist <- split(GS2Gene[,c("gs_name", "entrezid")], f = GS2Gene$gs_name)
     nGS <- length(GSlist)
     GSname <- names(GSlist)
-
-    w <- lapply(seq_len(nGS-1), function(x){
-        lapply((x+1):nGS, function(y){
-            data.frame(from = GSname[x], to = GSname[y], weight = jacIdex_func(GSlist[[x]]$entrezid, GSlist[[y]]$entrezid))
-        })
-    })
-
+    
+    w <- lapply(
+        seq_len(nGS - 1), 
+        function(x){
+            lapply(
+                seq(x + 1, nGS, by = 1),
+                function(y) {
+                    w <- jacIdex_func(GSlist[[x]]$entrezid, GSlist[[y]]$entrezid)
+                    data.frame(from = GSname[x], to = GSname[y], weight = w)
+                }
+            )
+        }
+    )
+    
     w <- bind_rows(lapply(w, bind_rows))
-    w <- filter(w, from != to)
-
+    w <- dplyr::filter(w, from != to)
+    
     g <- graph.data.frame(dplyr::select(w, from, to), directed = FALSE)
     g <- set_edge_attr(g, "weight", value = w$weight)
-
+    
     GSsize <- melt(lapply(GSlist, nrow))
     colnames(GSsize) <- c("size", "from")
     g <- set_vertex_attr(g, "size", index = GSsize$from, value = GSsize$size)
-
-    if (colorBy == "robustZ"){
-        GScolor <- mutate(GS2Gene, color =  ifelse(robustZ < 0, "Inhibited", "Activated"))
-        g <- set_vertex_attr(g, "color", index = GScolor$gs_name, value = GScolor$color)
+    
+    if (!is.null(colorBy)) {
+        GSvalue <- unique(GS2Gene[,c("gs_name", colorBy)])
+        g <- set_vertex_attr(
+            g, "color", index = GSvalue$gs_name, value = GSvalue[[colorBy]]
+        )
     }
-
-    if (colorBy == "pvalue"){
-        GSpvalue <- unique(GS2Gene[,c("gs_name", "pvalue")])
-        g <- set_vertex_attr(g, "color", index = GSpvalue$gs_name, value = GSpvalue$pvalue)
-    }
-
+    
     if(!plotIsolated){
         removeEdge <- which(E(g)$weight == 0)
         g <-  delete_edges(g, removeEdge)
         IsolatedNode <- which(degree(g) == 0)
         g <- delete_vertices(g, IsolatedNode)
     }
-
-
+    
     g
 }
 
-
-
-
 #' @importFrom reshape2 melt
+#' @keywords internal
 get_GSgenelist <- function(gsTopology, mapEntrezID = NULL){
     GStoGene <- lapply(gsTopology, rownames)
     GStoGene <- reshape2::melt(GStoGene)
     colnames(GStoGene) <- c("entrezid", "gs_name")
-
+    
     if (all(c("entrezid","mapTo") %in% colnames(mapEntrezID))){
         if (any(GStoGene$entrezid %in% mapEntrezID$entrezid)){
-            left_join(GStoGene, mapEntrezID[,c("entrezid","mapTo")], by = "entrezid")
+            left_join(
+                GStoGene, mapEntrezID[,c("entrezid","mapTo")], by = "entrezid"
+            )
         } else {
             warning("None of the Entrez IDs in mapEntrezID mapped to gsTopology.")
             return(GStoGene)
-
         }
     } else {
         return(GStoGene)
     }
-    }
+}
 
-
+#' @keywords internal
 jacIdex_func <- function(x, y) {
     x <- unlist(x)
     y <- unlist(y)
-    length(intersect(x, y))/length(unique(c(x,y)))
+    cmn <- intersect(x, y)
+    universe <- union(x, y)
+    length(cmn) / length(universe)
 }
 
-# This function was a modified version of the str_replace_nth function from martinctc/textworks
+#' @importFrom stringr str_split
+#' @keywords internal
 str_replace_nth <- function(x, pattern, replacement, n) {
-    g <- gregexpr(pattern, x)[[1]][n]
-    s <- scan(text = gsub("[()]", "", pattern),
-              sep = "|",
-              what = "",
-              quiet = TRUE)
-    substr(x, g, g) <- replacement[match(substr(x, g, g), s)]
-    x
+    x_list <- str_split(x, pattern = pattern)
+    l <- vapply(x_list, length, integer(1))
+    x_list[l > n] <- lapply(
+        x_list[l > n],
+        function(x){
+            index <- seq(n, length(x), by = n)
+            x[index] <- paste(x[index], replacement, sep = "")
+            not_index <- setdiff(seq_along(x), index)
+            x[not_index] <- paste(x[not_index], pattern, sep = "")
+            x <- paste(x, collapse = "")
+        }
+    )
+    x_list[l <= n] <- lapply(x_list[l <= n], paste, collapse = pattern)
+    unlist(x_list)
 }
