b32f027d6c895b37df776594706ef9774f54352c
Wenjun-Liu <a1680844@student.adelaide.edu.au>
2023-07-27 16:58:21 +0930

    fixed the counting of downstream genes

--- R/pathwayTopology.R
+++ R/pathwayTopology.R
@@ -12,12 +12,12 @@
 #' @return A list where each element is a matrix corresponding to a pathway
 #' @details
 #' This function takes pathway topology information retrieved using `graphite`
-#' and converts these to normalized weighted directed adjacency matrices 
+#' and converts these to normalized weighted directed adjacency matrices
 #' describing the gene signaling network, which can be used to compute gene-wise
-#' and pathway-level perturbation score through the scoring algorithm derived 
+#' and pathway-level perturbation score through the scoring algorithm derived
 #' from the *SPIA* algorithm. See cited document for more details.
 #'
-#' The `database` parameter may specify multiple databases but only one species 
+#' The `database` parameter may specify multiple databases but only one species
 #' can provided to the `species` parameter.
 #'
 #' Users can provide areas of interests as keywords, which will be matched to
@@ -35,10 +35,10 @@
 #' "binding/association_phosphorylation","dissociation_phosphorylation",
 #' "indirect_phosphorylation")`.
 #'
-#' If unspecified, beta will be set as an integer vector with: a) values of 1 
+#' If unspecified, beta will be set as an integer vector with: a) values of 1
 #' for interactions which match 'expression' or 'activation'; b) values of -1
-#' for interactions which match 'repression' or 'inhibition'; and c) 0 
-#' elsewhere. 
+#' for interactions which match 'repression' or 'inhibition'; and c) 0
+#' elsewhere.
 #' The converted weighted adjacent matrices will be stored in a list. We
 #' recommend users to store the returned list as a file so this step only
 #' needs to be performed once for each database.
@@ -67,7 +67,7 @@ retrieve_topology <-  function(
     ),
     species = c(
       "hsapiens", "athaliana", "btaurus", "celegans", "cfamiliaris",
-      "dmelanogaster", "drerio", "ecoli", "ggallus", "mmusculus", 
+      "dmelanogaster", "drerio", "ecoli", "ggallus", "mmusculus",
       "rnorvegicus", "scerevisiae", "sscrofa", "xlaevis"
     ),
     keyword = NULL, beta = NULL
@@ -77,19 +77,19 @@ retrieve_topology <-  function(
   ## pre-loaded nrmalised scores. We need to discuss
   database <- match.arg(database, several.ok = TRUE)
   species <- match.arg(species)
-  
+
   rel <- c(
     "activation", "compound", "binding/association", "expression",
     "inhibition", "activation_phosphorylation", "phosphorylation",
-    "inhibition_phosphorylation", "inhibition_dephosphorylation", 
-    "dissociation", "dephosphorylation", "activation_dephosphorylation", 
+    "inhibition_phosphorylation", "inhibition_dephosphorylation",
+    "dissociation", "dephosphorylation", "activation_dephosphorylation",
     "state change", "activation_indirect effect", "inhibition_ubiquination",
-    "ubiquination", "expression_indirect effect", 
-    "inhibition_indirect effect", "repression","dissociation_phosphorylation", 
+    "ubiquination", "expression_indirect effect",
+    "inhibition_indirect effect", "repression","dissociation_phosphorylation",
     "indirect effect_phosphorylation","activation_binding/association",
     "indirect effect","activation_compound","activation_ubiquination"
   )
-  
+
   if (is.null(beta)) {
     beta <- rep(0, length(rel))
     names(beta) <- rel
@@ -100,24 +100,23 @@ retrieve_topology <-  function(
       stop("Beta has wrong length or names. See details for requirements")
     }
   }
-  
+
   datpT <- .retrieveTopology(database, species, keyword)
   int2keep <- names(beta[beta != 0])
   datpT <- lapply(datpT, function(x) x[names(x) %in% int2keep] )
-  
+
   ## sometimes SPIA adds a list element called `<graphite_placeholder>`.
   ## Remove it if it exists
   datpT <- datpT[str_detect(names(datpT), "placeholder",  negate = TRUE) ]
-  
+
   gsTopology <- lapply(
-    names(datpT), 
+    names(datpT),
     function(x){
       g2gInteraction <- lapply(
         int2keep, function(y) datpT[[x]][[y]] * beta[y]
       )
       g2gInteraction <-   Reduce('+', g2gInteraction)
-      numDownstream <- Reduce('+', datpT[[x]])
-      numDownstream <- apply(numDownstream, 2, sum)
+      numDownstream <- apply(g2gInteraction , 1, function(x){sum(x!=0)})
       numDownstream[numDownstream == 0] <- 1
       B <- g2gInteraction/numDownstream
       diag(B) <- diag(B) - 1
@@ -126,20 +125,20 @@ retrieve_topology <-  function(
   )
   names(gsTopology) <- names(datpT)
   gsTopology
-  
+
 }
 
 #' @importFrom graphite pathways convertIdentifiers prepareSPIA
 .retrieveTopology <- function(database, species, keyword){
-  
+
   pys <- lapply(database, function(x) pathways(species, x))
   names(pys) <- database
-  
+
   # If keywords are provided, use pattern match to extract pathways whose
   # names match with the keywords
   if (!is.null(keyword)) {
     pys <- lapply(
-      pys, 
+      pys,
       function(x){
         pat <- paste(tolower(keyword), collapse = "|")
         ind <- grepl(pat, tolower(names(x)))
@@ -151,14 +150,14 @@ retrieve_topology <-  function(
   }
   # always convert pathway nodes identifier to entrez ID
   pys <- lapply(pys, convertIdentifiers, "ENTREZID")
-  
+
   # prepare the topologies for SPIA algorithm and store as a temporary file
   outputDir <- tempdir()
   lapply(database, function(x) prepareSPIA(pys[[x]], file.path(outputDir, x)))
-  
+
   # read the RData into the env
   ls <- lapply(
-    database, 
+    database,
     function(x){
       rd_name <- file.path(outputDir, paste0(x, "SPIA.RData"))
       get(load(rd_name))
@@ -166,5 +165,5 @@ retrieve_topology <-  function(
   )
   names(ls) <- database
   unlist(ls, recursive = FALSE)
-  
+
 }
